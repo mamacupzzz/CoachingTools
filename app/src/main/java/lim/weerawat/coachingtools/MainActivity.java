3f33cb64f00993af039d78b5fbd701ba24515b34
mama <phairin.cha.job@gmail.com>
2016-08-11 11:21:55 +0700

    Add Button

--- app/src/main/java/lim/weerawat/coachingtools/MainActivity.java
+++ app/src/main/java/lim/weerawat/coachingtools/MainActivity.java
@@ -1,7 +1,9 @@
 package lim.weerawat.coachingtools;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }   //Main Methood
+
+
+    public void clickSignUpMain(View view) {
+        startActivity(new Intent(MainActivity.this,SignUPActivity.class));
     }
-}
+}   //Main Class
+
