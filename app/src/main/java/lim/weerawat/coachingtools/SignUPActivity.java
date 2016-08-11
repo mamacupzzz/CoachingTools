571fd50d05264debb153c8bd85c358cb80499742
mama <phairin.cha.job@gmail.com>
2016-08-11 11:49:03 +0700

    UI SignUp Done

--- app/src/main/java/lim/weerawat/coachingtools/SignUPActivity.java
+++ app/src/main/java/lim/weerawat/coachingtools/SignUPActivity.java
@@ -9,5 +9,9 @@ public class SignUPActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
-    }
-}
+    }   //Main Method
+
+
+
+
+}   //MainClass
