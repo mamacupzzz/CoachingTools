07e48522e799016c9159419f7b10e8b5f9d46cb9
mama <phairin.cha.job@gmail.com>
2016-08-11 15:31:05 +0700

    Import JSON to String

--- app/src/main/java/lim/weerawat/coachingtools/MainActivity.java
+++ app/src/main/java/lim/weerawat/coachingtools/MainActivity.java
@@ -1,10 +1,18 @@
 package lim.weerawat.coachingtools;
 
+import android.content.Context;
 import android.content.Intent;
+import android.os.AsyncTask;
+import android.speech.tts.Voice;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.Response;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -27,6 +35,46 @@ public class MainActivity extends AppCompatActivity {
 
     }   //Main Methood
 
+    //Create Inner Class
+    private class SynUser extends AsyncTask<Void, Void, String> {
+        //Explicit
+        private Context context;
+        private String myUserString, myPasswordString;
+        private static final String urlJON = "http://swiftcodingthai.com/mama/get_data_mama.php";
+
+        public SynUser(Context context, String myUserString, String myPasswordString) {
+            this.context = context;
+            this.myUserString = myUserString;
+            this.myPasswordString = myPasswordString;
+        }
+
+        @Override
+        protected String doInBackground(Void... params) {
+
+            try {
+                OkHttpClient okHttpClient = new OkHttpClient();
+                Request.Builder builder = new Request.Builder();
+                Request request = builder.url(urlJON).build();
+                Response response = okHttpClient.newCall(request).execute();
+                return response.body().string();
+
+
+            } catch (Exception e) {
+                return null;
+            }
+
+        }   //Network BackEnd
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+
+            Log.d("11AugV1", "JSON ==>" + s);
+
+        }   // Posting
+    }   //SynUser Class
+
+
     public void clickSignIn(View view) {
         //Get Value
         userString = usereditText.getText().toString().trim();
@@ -42,6 +90,8 @@ public class MainActivity extends AppCompatActivity {
 
         } else {
             //No Space
+            SynUser synUser = new SynUser(this,userString,passwordString);
+            synUser.execute();
 
         }
     }    //ClickSignIN
