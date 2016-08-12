f18da70689318cd1c690dc7e1d920ac4b87768e0
mama <phairin.cha.job@gmail.com>
2016-08-12 11:35:42 +0700

    vdo choose and upload

--- app/src/main/java/lim/weerawat/coachingtools/ServiceActivity.java
+++ app/src/main/java/lim/weerawat/coachingtools/ServiceActivity.java
@@ -1,13 +1,34 @@
 package lim.weerawat.coachingtools;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class ServiceActivity extends AppCompatActivity {
+
+    //Explicit
+    private static final int myINT = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_service);
+    }    //Main Method
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
     }
-}
+
+    public void clickupload(View view) {
+        //Choose VDO
+        Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+        intent.setType("video/*");
+        startActivityForResult(Intent.createChooser(intent,"Select vdo"),myINT);
+
+
+
+
+    }   //Upload VOD
+}   //Main Class
